1d34ee02dfba6beb5a096d16d6471a766e9a8edb
IbukiShinzato <e235718@ie.u-ryukyu.ac.jp>
2026-09-17 11:36:58 +0900

    feat(driver): add UART output

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,22 @@ use core::panic::PanicInfo;
 
 global_asm!(include_str!("../entry.S"));
 
+const UART_BASE: usize = 0x1000_0000;
+
+fn putc(c: u8) {
+    unsafe {
+        (UART_BASE as *mut u8).write_volatile(c);
+    }
+}
+
+fn puts(s: &str) {
+    s.bytes().for_each(putc);
+}
+
 #[unsafe(no_mangle)]
 pub extern "C" fn kernel_main() -> ! {
+    puts("\nHello, Partisan!\n");
+
     loop {}
 }
 
